520ff55c037be06cd736630f3a63f2f695fcc73e
Nitesh <niteshsengar9831@gmail.com>
2024-01-20 02:01:07 +0530

    Complete Queue program provide extra features to use complete space of queue

--- Queue_array.cpp
+++ Queue_array.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 // Queue using array 
+// This is very special because you can use the free space of array when item is removed in a circular manner but
+// from outside it will look normal queue
 
 class Queue
 {
@@ -13,6 +15,9 @@ class Queue
        void CreateQueue(int);
 
        void Insertion(int);
+        
+        void Deletion();
+       void printQueue();
 
 };
 
@@ -38,15 +43,109 @@ void Queue::Insertion(int data)
     // Queue is empty
     if(front==-1 && rear==-1)
     {
-        ptr[0]=data;
         front=0;
         rear=0;
+        ptr[rear]=data;
+
+    }
+    else if(front == rear)
+    {
+        rear ++;
+        ptr[rear]=data;
+    }
+    else if(rear<capacity-1 && rear > front)
+    {
+        rear++;
+        ptr[rear]=data;
+    }
+    else if(rear==capacity-1) 
+    {
+    // when rear is in the last but queue is full
+        if(front==0)
+        {
+             cout<<"Que is overflow"<<endl;
+        }
+        // when rear is in the last index and queue is empty from first
+        else
+        {
+            rear = 0;
+            ptr[rear] = data;
+        }
+    }
+    else if(rear < front && rear != front - 1 )
+    {
+        rear ++;
+        ptr[rear]= data;
+    }
+    else if(rear == front - 1)
+    {
+        cout<<"Que is overflow"<<endl;
+    }
+}
+
+void Queue::Deletion()
+{
+    if(front == -1 && rear == -1)
+    {
+        cout<<"Queue is empty"<<endl;
+    }
+    // only one value is in queue
+    else if(front == rear)
+    {
+        front = -1;
+        rear = -1;
+    }
+    else if(front<rear)
+    {
+        front++;
+    }
+    else if(rear<front)
+    {
+        if(front!=capacity-1)
+            front++;
+        else
+            front =0;
+    }
+}
+
+void Queue::printQueue(){
+    if(rear>front){
+    for(int i=front;i<=rear;i++){
+        cout<<ptr[i]<<endl;
+    }
+    }
+    else{
+        for(int i = front ;i<capacity;i++)
+        {
+            cout<<ptr[i]<<endl;
+        }
+        for(int i=0;i<=rear;i++)
+        {
+            cout<<ptr[i]<<endl;
+        }
     }
 }
 
 
-
 int main()
 {
-
+    Queue q1;
+    q1.CreateQueue(5);
+    q1.Insertion(10);
+    q1.Insertion(20);
+    q1.Insertion(30);
+    q1.Insertion(40);
+    q1.Insertion(50);
+    q1.Deletion();
+    q1.Deletion();
+    q1.Insertion(60);
+    q1.Insertion(70);
+    q1.Insertion( 80);
+    q1.Deletion();
+     q1.Insertion( 80);
+      q1.Deletion();
+    q1.Deletion();
+    q1.Insertion(90);
+    q1.printQueue();
+    
 }
